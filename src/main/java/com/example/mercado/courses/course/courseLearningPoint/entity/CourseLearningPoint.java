d454fcdf3d5b208cc4d6dcde3f5156ba6c4b507a
squizzysw <dmpoleg2006@gmail.com>
2026-04-05 20:05:06 +0200

    feat(course): add CourseRequirement and CourseLearningPoint entity

---
+++ src/main/java/com/example/mercado/courses/course/courseLearningPoint/entity/CourseLearningPoint.java
@@ -0,0 +1,37 @@
+package com.example.mercado.courses.course.courseLearningPoint.entity;
+
+import com.example.mercado.courses.course.common.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(
+        name = "course_learning_points",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_position_course_id",
+                        columnNames = {"course_id", "position"}
+                )
+        }
+)
+public class CourseLearningPoint extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "course_id", nullable = false)
+    private Long courseId;
+
+    @Column(name = "text", nullable = false, length = 1000)
+    private String text;
+
+    @Column(name = "position", nullable = false)
+    private Integer position;
+
+}
